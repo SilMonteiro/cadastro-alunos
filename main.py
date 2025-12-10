2f41a802a31486f035f1359261950b9b0259df0d
Silvia Monteiro <silvia.monteiro800@gmail.com>
2025-12-09 21:12:04 -0300

    Dataframe01
    Criando Dataframe e criando a opção para salvar os dados

---
+++ main.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import random
+
+def gerar_df()
+    
+    try:
+        df = pd.read_csv('alunos.csv')
+    except FileNotFoundError:
+        df = pd.DataFrame(Columns=["Matricula", "Nome", "Rua", "Número", "Bairro", "Cidade", "UF", "Telefone", "e-mail"])
+
+    return df
+
+
+def salvando_dados(df):
+
+    df.to_csv('alunos.csv', index=False)
+
+    
+
